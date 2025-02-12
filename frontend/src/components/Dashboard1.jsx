1a3def09ee8d9706ece25f500e55bbb77cfac6c2
Alan Andrade <alannog02@gmail.com>
2025-02-12 14:13:28 -0300

    30%

---
+++ frontend/src/components/Dashboard1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dashboard1 = () => {
+    return(
+        <div>Página 1 do Dashboard1</div>
+    )
+}
+
+export default Dashboard1
---
+++ frontend/src/components/Dashboard2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dashboard2 = () => {
+    return(
+        <div>Página 1 do Dashboard2</div>
+    )
+}
+
+export default Dashboard2
